64f770f9d0f159a2b8990c7334dcff19f8e01aec
hexu <hexu_1985@sina.com>
2023-04-27 18:10:45 +0800

    add base_128_varint_encode.py

---
+++ data_type_and_encoding/base_128_varint/cxx/base_128_varint_encode.py
@@ -0,0 +1,22 @@
+import sys
+
+def base_128_varint_encode(istr):
+    bits=bin(int(istr,10))[2:]
+    bits_list=[]
+    while len(bits) > 7:
+        bits_list.append('1'+bits[-7:])
+        bits=bits[:-7]
+    bits_list.append((8-len(bits))*'0'+bits) 
+    return bits_list
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("usage: {} n(base 10)".format(sys.argv[0]))
+        print("\tprint result of base-128-varint encoding")
+        sys.exit(1)
+
+    bits_list = base_128_varint_encode(sys.argv[1])
+    print(bits_list)
+    for bits in bits_list:
+        print("{:02x}".format(int(bits, 2)), end=" ")
+    print()
