7c17a50ef377dc92431515ebbb6ab211baf71ee4
hexu <hexu_1985@sina.com>
2024-06-18 17:00:20 +0800

    update getting_started/addressbook/cxx

--- getting_started/addressbook/cxx/build_proto.sh
+++ getting_started/addressbook/cxx/build_proto.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 current_file_path=$(cd `dirname ${BASH_SOURCE[0]}` && pwd)
-SRC_DIR=${current_file_path}/../proto
-DST_DIR=${current_file_path}/cxx_proto
+SRC_DIR=${current_file_path}/proto
+DST_DIR=${current_file_path}/generated
 
 mkdir -p $DST_DIR
 protoc -I=$SRC_DIR --cpp_out=$DST_DIR $SRC_DIR/addressbook.proto
