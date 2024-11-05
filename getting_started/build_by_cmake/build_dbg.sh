2c533155581f5591c4ca9532b7744b7319548662
xu.he <xu.he@phigent.ai>
2024-11-05 17:49:13 +0800

    update getting_started/build_by_cmake

--- getting_started/build_by_cmake/build_dbg.sh
+++ getting_started/build_by_cmake/build_dbg.sh
@@ -1,4 +1,4 @@
 cmake -Hcmake -Bbuild \
-    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DCMAKE_INSTALL_PREFIX=~/local/protobuf
 VERBOSE=1 cmake --build build --parallel 4 --target install
